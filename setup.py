b72b9924096f129e4d87cbf9bce7dc66d4156baa
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2012-08-28 16:31:23 +0300

    added setup

---
+++ setup.py
@@ -0,0 +1,51 @@
+"""
+wtforms-alchemy
+-------------------
+
+Generates WTForms forms from SQLAlchemy models.
+"""
+
+from setuptools import setup, Command
+import subprocess
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call(['py.test'])
+        raise SystemExit(errno)
+
+setup(
+    name='wtforms-alchemy',
+    version='0.1.0',
+    url='https://github.com/kvesteri/wtforms-alchemy',
+    license='BSD',
+    author='Konsta Vesterinen',
+    author_email='konsta@fastmonkeys.com',
+    description='Generates WTForms forms from SQLAlchemy models.',
+    long_description=__doc__,
+    packages=['wtforms_json'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'WTForms>=1.0.1'
+    ],
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
