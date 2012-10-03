26ecbead9baf707ab2f0c3b1b628b4f4a525e3ef
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2012-10-03 12:41:22 +0300

    bumped version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class PyTest(Command):
 
 setup(
     name='wtforms-json',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/kvesteri/wtforms-json',
     license='BSD',
     author='Konsta Vesterinen',
